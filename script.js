a4dcb6413a4a712b12483a7e5bd10a972c2d06c7
Eliel Delfino <153773937+ElielDelfino@users.noreply.github.com>
2024-09-13 01:01:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+// tenho que verificar se o indice puxado é igual ao indice final 
+function isPalindrome(str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, "")
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] !== str[str.length - 1 - i]) {
+            return false;
+        }
+    } return true;
 }
 
-function arrayMaxMin(arr){
+
+function arrayMaxMin(arr) {
     /* Seu código aqui */
 }
