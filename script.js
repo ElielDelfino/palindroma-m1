41447047bdc7434edaf1e1419989cefb8f399c19
Eliel Delfino <153773937+ElielDelfino@users.noreply.github.com>
2024-09-13 13:17:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-// tenho que verificar se o indice puxado é igual ao indice final 
+
 function isPalindrome(str) {
     str = str.toLowerCase().replace(/[^a-z0-9]/g, "")
-    for (let i = 0; i < str.length / 2; i++) {
-        if (str[i] !== str[str.length - 1 - i]) {
+    let fraseInvertida = str.split("").reverse().join("")
+    
+    
+    for (let i = 0; i < str.length ; i++) {
+        if (str[i] != fraseInvertida[i]) {
             return false;
         }
-    } return true;
+    } 
+    return true;
 }
 
 
 function arrayMaxMin(arr) {
-    /* Seu código aqui */
-}
+    let arrayMax = arr[0];
+    let arrayMin = arr[0];
+    
+    for (let i = 1 ;  i < arr.length ; i++){
+        if(arr[i] > arrayMax){
+            arrayMax = arr[i];
+        }
+        if(arr[i] < arrayMin){
+            arrayMin = arr[i]
+        }
+         }return [arrayMin, arrayMax]
+    } 
+
+
+
